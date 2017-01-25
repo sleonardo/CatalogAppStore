f0b148dc33696093ec0f3bf3d0c6b69a9ea4e952
sleonardo <simozaleonardo@gmail.com>
2017-01-25 01:23:02 -0400

    Service Configuration

---
+++ rappi/GridFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  GridFlowLayout.swift
+//  rappi
+//
+//  Created by Leonardo Simoza on 24/1/17.
+//  Copyright © 2017 Rappi. All rights reserved.
+//
+
+import Foundation
---
+++ rappi/ListFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  ListFlowLayout.swift
+//  rappi
+//
+//  Created by Leonardo Simoza on 24/1/17.
+//  Copyright © 2017 Rappi. All rights reserved.
+//
+
+import Foundation
